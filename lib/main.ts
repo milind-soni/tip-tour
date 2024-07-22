fb78b1450afa86fbb1a973f2f4dae1cd093c7c36
ABSanthosh <a.b.santhosh02@gmail.com>
2024-07-23 00:11:09 +0530

    chore: entry file

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,15 @@
-console.log('Hello, world!');
-console.log('Hello, world!');
+import FollowBox from "./utils/FollowBox";
+import Overlay from "./utils/Overlay";
+
+// All data attributes in current script
+// const scriptTag = document.currentScript;
+// const dataAttributes = scriptTag?.dataset as {
+//   token: string;
+// };``
+
+document.addEventListener("mousemove", Overlay);
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  const followBox = new FollowBox();
+  followBox.createFollowBox();
+});
