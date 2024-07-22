f34387a60fe939f86b505a44f04078b5b89c9403
ABSanthosh <a.b.santhosh02@gmail.com>
2024-07-23 00:11:59 +0530

    chore: follow box styles

---
+++ lib/utils/FollowBox/style.ts
@@ -0,0 +1,12 @@
+export const styles = {
+  width: "270px",
+  height: "90px",
+  border: "1px solid #ccc",
+  "background-color": "#f8f8f8",
+  position: "absolute",
+  transform: "translate(-50%, -50%)",
+  "border-radius": "4px",
+  "box-shadow": "0 0 20px rgba(16, 0, 54, 0.2)",
+  transition: "0.1s ease-out",
+  "z-index": "1000",
+};
